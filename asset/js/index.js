70a868e6f8e7d6ba616a9669a2ddaf02039f94e8
Emmanuel Appiagyei <emmanuelappiagyei@cmail.carleton.ca>
2026-05-17 13:10:41 -0400

    dynamically rendered all apparels using js

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,2 +1,42 @@
 import apparelArr from './data.js';
 
+const apparelEl = document.getElementById('menu');
+
+const createApparelList = (arr) => {
+    arr.forEach( apparel => {
+        const apparelsContainer = document.createElement('div');
+        apparelsContainer.classList.add('apparels-container');
+
+        const apparelEmojiContainer =  document.createElement('div');
+        const apparelInfoContainer =  document.createElement('div');
+        const addButtonEl = document.createElement('button');
+
+        const itemName = document.createElement('p');
+        const itemMaterials = document.createElement('p');
+        const itemPrice = document.createElement('p');
+
+        const {name, materials, price, emoji} = apparel;
+        apparelEmojiContainer.textContent = emoji;
+        itemName.textContent = name;
+        // itemMaterials.textContent = 
+        itemPrice.textContent = `$${price}`;
+        addButtonEl.textContent = '+';
+
+
+        apparelInfoContainer.appendChild(itemName);
+        apparelInfoContainer.appendChild(itemPrice);
+
+
+        apparelsContainer.appendChild(apparelEmojiContainer);
+        apparelsContainer.appendChild(apparelInfoContainer);
+        apparelsContainer.appendChild(addButtonEl);
+
+
+        apparelEl.appendChild(apparelsContainer);
+    })
+
+}
+
+
+
+createApparelList(apparelArr);
