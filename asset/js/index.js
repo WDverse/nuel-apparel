55887c21d500304a0b54aee6e77556fa05530bd3
Emmanuel Appiagyei <emmanuelappiagyei@cmail.carleton.ca>
2026-05-21 21:14:26 -0400

    added feature create precheckout section and render item name when corresponding add-btn is cliked

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,6 +1,7 @@
 import apparelArr from './data.js';
 
 const apparelEl = document.getElementById('menu');
+const orderList = document.getElementById('order-list');
 
 const createApparelList = (arr) => {
     arr.forEach( apparel => {
@@ -16,11 +17,14 @@ const createApparelList = (arr) => {
         const itemPrice = document.createElement('p');
 
         const {name, materials, price, emoji} = apparel;
+        apparelsContainer.dataset.name = name // add data-* attr to every item container 
         apparelEmojiContainer.textContent = emoji;
         itemName.textContent = name;
         itemMaterials.textContent = materials.map(material => material).join(', ');
         itemPrice.textContent = `$${price}`;
         addButtonEl.textContent = '+';
+
+        addButtonEl.addEventListener('click', handlePreCheckout)
 
         apparelInfoContainer.appendChild(itemName);
         apparelInfoContainer.appendChild(itemMaterials);
@@ -35,6 +39,14 @@ const createApparelList = (arr) => {
 
 };
 
+function handlePreCheckout (e) {
+    const orderListContainer = document.createElement('div');
+    const addedItemName = document.createElement('span');
+
+    addedItemName.textContent = e.target.parentElement.dataset.name;
+    orderListContainer.appendChild(addedItemName);
+    orderList.appendChild(orderListContainer);
+}
 
 
 createApparelList(apparelArr);
